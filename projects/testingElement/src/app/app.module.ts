e598e9c7b8b3e3cd45e86d873a6588a47872bc7c
BMO_USER <earnestcpy@gmail.com>
2019-05-24 13:37:53 -0400

    test screen with testElement initialed

---
+++ projects/testingElement/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement} from '@angular/elements';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [],
+  entryComponents:[
+    AppComponent
+
+  ]
+})
+export class AppModule { 
+  constructor(private injector: Injector){
+
+  }
+  ngDoBootstrap(){
+    const appElement = createCustomElement(AppComponent, {injector: this.injector})
+    customElements.define('sidebar', appElement);
+  }
+}
+
